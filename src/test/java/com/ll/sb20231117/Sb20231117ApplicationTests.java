803a83365b1e7d42554d30bd332cd84d85a7528d
J-Cheol <lunaris0206@naver.com>
2023-11-17 01:04:19 +0900

    start

---
+++ src/test/java/com/ll/sb20231117/Sb20231117ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.sb20231117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20231117ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
