63f49db15fb64a44773d1a3c5e00c36f810c959e
J-Cheol <lunaris0206@naver.com>
2023-12-01 23:50:08 +0900

    t2

--- src/test/java/com/ll/sb20231117/Sb20231117ApplicationTests.java
+++ src/test/java/com/ll/sb20231117/Sb20231117ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ll.sb20231117;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,4 +14,10 @@ class Sb20231117ApplicationTests
         int i = 0;
     }
 
+    @Test
+    @DisplayName("t2")
+    void t2()
+    {
+        String test1;
+    }
 }
